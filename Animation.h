aa9c67968274201b8a7585d5554499415ac78cee
BaburPadishah <51137224+BaburPadishah@users.noreply.github.com>
2020-04-28 12:23:33 -0700

    Animation Class

---
+++ Animation.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+
+class Animation
+{
+public:
+	Animation(sf::Texture* texture, sf::Vector2u imageCount, float switchTime);
+	~Animation();
+
+	void update(int row, float deltaTime);
+
+	sf::IntRect uvRect; // rectangular subset of the texture
+
+private:
+	sf::Vector2u imageCount; // number of images you wish to cycle through
+	sf::Vector2u currentImage;
+	float totalTime;
+	float switchTime; // time to switch between frames
+
+};

27353e5c7b99d44ba90069cc706d6e14dbadf5a6
BaburPadishah <51137224+BaburPadishah@users.noreply.github.com>
2020-04-28 12:22:12 -0700

    ladybug animation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <SFML\Graphics.hpp>
+#include <SFML\Audio.hpp>
 #include <iostream>
+#include "Animation.h"
 
 using std::cout;
 
@@ -20,16 +22,34 @@ int main()
 	bgSprite.setScale(10.0f, 10.0f);
 
 
-	sf::Texture bug;
-	bug.loadFromFile("png/bug_sheet.png");
-	sf::Sprite bugSprite;
-	bugSprite.setTexture(bug);
-	//bugSprite.setScale(10.0f, 10.0f);
+	sf::Texture lady;
+	lady.loadFromFile("png/lady.png");
+	sf::Sprite ladySprite;
+	ladySprite.setTexture(lady);
+	ladySprite.setScale(10.0f, 10.0f);
 
-	bool inLunge;
+	Animation animation(&lady, sf::Vector2u(3, 1), 0.3f);
+	float deltaTime = 0.0f;
+	sf::Clock clock;
+
+	sf::Texture beetle;
+	beetle.loadFromFile("png/beetle.png");
+	sf::Sprite beetleSprite;
+	beetleSprite.setTexture(beetle);
+	beetleSprite.setScale(10.0f, 10.0f);
+
+	sf::Texture worm;
+	worm.loadFromFile("png/worm.png");
+	sf::Sprite wormSprite;
+	wormSprite.setTexture(worm);
+	wormSprite.setScale(10.0f, 10.0f);
+
+	bool inLunge = false;
 
 	while (window.isOpen())
 	{
+		deltaTime = clock.restart().asSeconds();
+
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -49,9 +69,11 @@ int main()
 					{
 						inLunge = true;
 					}
-					if ((event.key.code == sf::Keyboard::Right ||
-						event.key.code == sf::Keyboard::D) &&
-						!inLunge) 
+					if ((event.key.code == sf::Keyboard::Left ||
+						event.key.code == sf::Keyboard::A) &&
+						(event.key.code == sf::Keyboard::Down ||
+						event.key.code == sf::Keyboard::S) &&
+						!inLunge)
 					{
 						inLunge = true;
 					}
@@ -61,14 +83,31 @@ int main()
 					{
 						inLunge = true;
 					}
+					if ((event.key.code == sf::Keyboard::Down ||
+						event.key.code == sf::Keyboard::S) &&
+						(event.key.code == sf::Keyboard::Right ||
+						event.key.code == sf::Keyboard::D) &&
+						!inLunge)
+					{
+						inLunge = true;
+					}
+					if ((event.key.code == sf::Keyboard::Right ||
+						event.key.code == sf::Keyboard::D) &&
+						!inLunge)
+					{
+						inLunge = true;
+					}
 
 			}
 		}
 
+		animation.update(0, deltaTime);
+		ladySprite.setTextureRect(animation.uvRect);
+
 		window.clear(sf::Color::Black);
 
 		window.draw(bgSprite);
-		window.draw(bugSprite);
+		window.draw(ladySprite);
 
 		window.display();
 	}

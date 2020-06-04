d1374a9e98f92bde775072362e67336f7073bf1d
BaburPadishah <51137224+BaburPadishah@users.noreply.github.com>
2020-06-03 18:26:29 -0700

    Spider movement refined
    The spider now only lunges when the space bar is pressed. It lunges quickly, then retracts back into its burrow slowly.

--- Spider.cpp
+++ Spider.cpp
@@ -1,4 +1,5 @@
 #include "Spider.h"
+#include <iostream>
 
 Spider::Spider(sf::Texture* texture, sf::Vector2u imageCount, 
 	float switchTime) :
@@ -7,29 +8,11 @@ Spider::Spider(sf::Texture* texture, sf::Vector2u imageCount,
 	row = 2;
 
 	body.setTexture(*texture);
-}
 
-void Spider::update(float deltaTime)
-{
-	// sf::Keyboard::isKeyPressed(key) is a function to see of a particular key was pressed
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || 
-		sf::Keyboard::isKeyPressed(sf::Keyboard::Left) &&
-		row != 0 )
-	{
-		row--;
-	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) ||
-		sf::Keyboard::isKeyPressed(sf::Keyboard::Right) &&
-		row < 4)
-	{
-		row++;
-	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-	{
-		lunge(deltaTime);
-	}
-
-	animation.update(row, deltaTime, false);
+	sf::Vector2u image = animation.getCurrentImage();
+	image.y = row;
+	animation.setCurrentImage(image);
+	animation.uvRect.top = image.y * animation.uvRect.height;
 	body.setTextureRect(animation.uvRect);
 }
 
@@ -38,9 +21,54 @@ void Spider::draw(sf::RenderWindow& window)
 	window.draw(body);
 }
 
-void Spider::lunge(float deltaTime)
+bool Spider::lunge(float deltaTime, bool* delayed)
 {
+	// we must cycle through only once 
+	// set switch time to three times its value in frames 4 and 5
+	unsigned int prevImage = animation.getCurrentImage().x;
 	animation.update(row, deltaTime, false);
+	unsigned int curImage = animation.getCurrentImage().x;
+	body.setTextureRect(animation.uvRect);
+
+	if (curImage == 0 && prevImage != 0) // we've cycled through once
+	{
+		animation.setSwitchTime(0.05f); // had to hardcode switchTime, bad form
+		*delayed = false;
+		std::cout << "Spider switchTime has been reset" << std::endl;
+		return false;
+	}
+	else if (curImage == 4 && !(*delayed)) // lengthen time between frames by a certain factor
+	{
+		animation.setSwitchTime(1.5f); // hardcoding, bad form
+		*delayed = true;
+		std::cout << "Spider switchTime has been extended." << std::endl;
+	}
+	return true;
+}
+
+void Spider::shift(int direction)
+{
+	// add direction to row
+	row += direction;
+
+	//update animation's uvRect - get current image, change y component, set current image
+	sf::Vector2u image = animation.getCurrentImage();
+	image.y = row;
+	animation.setCurrentImage(image);
+	animation.uvRect.top = image.y * animation.uvRect.height;
+
+	//set new body rect
+	body.setTextureRect(animation.uvRect);
+}
+
+unsigned int Spider::getRow() const
+{
+	return row;
+}
+
+void Spider::setRow(unsigned int newRow)
+{
+	row = newRow;
 }
 
 Spider::~Spider()
--- Spider.h
+++ Spider.h
@@ -12,11 +12,19 @@ public:
 	//	animation update function: 
 	//	start in resting position 
 	//	shift resting position on left/right (change row)
-	//	lunge on spacebar: loop through frames 1-5, lingering on frame 5, 
+	//	lunge on spacebar: loop through frames 1-5, lingering on frame 4 and 5, 
 	//		then if bug is caught: show the closed hatch without legs for a few seconds, update health
 	//		if bug isn't caught, go back to resting position
-	void update(float deltaTime);
-	void lunge(float deltaTime);
+	bool lunge(float deltaTime, bool* delayed);
+
+	/**
+		Shifts the resting pose of the spider right or left
+
+		@param direction the direction to shift, -1 for left, 1 for right
+	*/
+	void shift(int direction);
+	unsigned int getRow() const;
+	void setRow(unsigned int newRow);
 
 	void draw(sf::RenderWindow& window);
 	void setScale(float x, float y) { body.setScale(x, y); };
@@ -27,5 +35,4 @@ private:
 	sf::Sprite body;
 	Animation animation;
 	unsigned int row;
-	bool inLunge;
 };
